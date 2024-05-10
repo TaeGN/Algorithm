b3c0bc80597194bca0188e4286308f23f194b6d7
taegn <taegyunn123@gmail.com>
2024-05-10 20:39:56 +0900

    add 로컬함수 예제 추가

---
+++ src/test/로컬함수.kt
@@ -0,0 +1,23 @@
+package test
+
+data class User(val id: Long, val name: String, val address: String)
+
+fun main() {
+    val user1 = User(1L, "aa", "aa@aa.com")
+    saveUser(user1)
+}
+
+fun saveUser(user: User) {
+    user.validateBeforeSave()
+    println(user)
+}
+fun User.validateBeforeSave() {
+    fun validate(value: String, fieldName: String) {
+        if(value.isEmpty()) {
+            throw IllegalArgumentException("$fieldName error")
+        }
+    }
+
+    validate(name, "Name")
+    validate(address, "address")
+}
