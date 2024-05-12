8431e1fb731876e628dc379a25afe0d1e2ec8c7c
taegn <taegyunn123@gmail.com>
2024-05-12 16:57:24 +0900

    add 클래스위임 예시 추가

---
+++ src/test/클래스위임.kt
@@ -0,0 +1,22 @@
+package test
+
+class CountingSet<T>(val innerSet: MutableCollection<T> = HashSet()): MutableCollection<T> by innerSet {
+    var addCount = 0
+    override fun add(element: T): Boolean {
+        addCount++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        addCount += elements.size
+        return innerSet.addAll(elements)
+    }
+}
+
+fun main() {
+    val set = CountingSet<Int>()
+    set.add(1)
+    println(set.addCount)
+    set.addAll(arrayOf(1,2,3,4,5))
+    println(set.addCount)
+}
