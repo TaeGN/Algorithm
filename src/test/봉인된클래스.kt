17f0f16200c76e62564e110609559f36aa25265b
taegn <taegyunn123@gmail.com>
2024-05-12 16:13:07 +0900

    add 봉인된클래스 예시 추가

---
+++ src/test/봉인된클래스.kt
@@ -0,0 +1,18 @@
+package test
+
+sealed class Expr {
+    class Num(val value: Int): Expr()
+    class Sum(val left: Expr, val right: Expr): Expr()
+}
+
+fun main() {
+    val num1 = Expr.Num(5)
+    val num2 = Expr.Num(7)
+    val sum = Expr.Sum(num1, num2)
+    println(eval(sum))
+}
+
+fun eval(e: Expr): Int = when(e) {
+    is Expr.Num -> e.value
+    is Expr.Sum -> eval(e.left) + eval(e.right)
+}
