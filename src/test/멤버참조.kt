cf2dbaf7f3108c4d45729fa3480681f0dc4bc089
taegn <taegyunn123@gmail.com>
2024-05-13 16:14:08 +0900

    add 멤버참조 예제 추가

---
+++ src/test/멤버참조.kt
@@ -0,0 +1,17 @@
+package test
+
+import kotlin.reflect.KProperty0
+
+data class Person(val name: String, val age: Int) {
+    fun info() = "${name}님의 나이는 ${age}입니다."
+}
+
+fun main() {
+    val people = listOf(Person("aa", 3), Person("bb", 9))
+    val infoList1 = people.map { it.info() }
+    val infoList2 = people.map(Person::info)
+    val infoList3 = people.map(Person::name)
+    println(infoList1)
+    println(infoList2)
+    println(infoList3)
+}
