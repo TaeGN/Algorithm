ee42cb5415012044bc1fce11eefbd9b58ddc3b88
taegn <taegyunn123@gmail.com>
2024-05-13 17:36:56 +0900

    add collection 지연계산 예제 추가

---
+++ src/Kotlin/collection/지연계산.kt
@@ -0,0 +1,9 @@
+package Kotlin.collection
+
+fun main() {
+    val list = listOf(1, 2, 3, 4)
+    val result1 = list.stream().filter { it > 2 }.map { it * it }.toList()
+    val result2 = list.asSequence().filter { it > 2 }.map { it * it }.toList()
+    println(result1)
+    println(result2)
+}
