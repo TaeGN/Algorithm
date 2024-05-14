06d23a33ec139f003cf9e510228d0e0af9c6a720
taegn <taegyunn123@gmail.com>
2024-05-14 22:52:21 +0900

    add 연산자오버로딩 예제 추가

---
+++ src/Kotlin/연산자오버로딩.kt
@@ -0,0 +1,22 @@
+package Kotlin
+
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point): Point {
+        return Point(x + other.x, y + other.y)
+    }
+}
+/**
+ * 확장함수로 (-)연산자 정의
+ */
+operator fun Point.minus(other: Point) : Point {
+    return Point(x - other.x, y - other.y)
+}
+
+fun main() {
+    val p1 = Point(1,2)
+    val p2 = Point(3,4)
+    val p3 = p1 + p2
+    val p4 = p1 - p2
+    println(p3)
+    println(p4)
+}
