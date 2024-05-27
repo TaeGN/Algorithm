50374b2c452bd86e3762663ed78226b70dafeee8
taegn <taegyunn123@gmail.com>
2024-05-27 18:31:55 +0900

    update p9663_NQueen.kt

--- src/백준/G4/p9663_NQueen/p9663_NQueen.kt
+++ src/백준/G4/p9663_NQueen/p9663_NQueen.kt
@@ -2,40 +2,45 @@ package 백준.G4.p9663_NQueen
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import kotlin.math.min
+import kotlin.math.abs
+
+data class Point(val r: Int, val c: Int) {
+    companion object {
+        lateinit var pointMap: List<List<Point>>
+        fun of(r: Int = 0, c: Int = 0) = pointMap[r][c]
+    }
+}
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val n = readLine().toInt()
-    val board = List(n) { MutableList(n) { false } }
-    println(nQueen(board = board))
+    Point.pointMap = List(n) { r -> List(n) { c -> Point(r, c) } }
+    val selected = MutableList(n) { Point.of() }
+    println(nQueen(selected = selected))
 }
 
-fun nQueen(r: Int = 0, board: List<MutableList<Boolean>>): Int {
-    if (r == board.size) return 1
+fun nQueen(r: Int = 0, selected: MutableList<Point>): Int {
+    if (r == selected.size) return 1
 
     var count = 0
-    for (c in board.indices) {
-        if (isValid(r, c, board)) {
-            board[r][c] = true
-            count += nQueen(r + 1, board)
-            board[r][c] = false
+    for (c in selected.indices) {
+        val point = Point.of(r, c)
+        if (isValid(r, c, selected)) {
+            selected[r] = point
+            count += nQueen(r + 1, selected)
         }
     }
 
     return count
 }
 
-fun isValid(r: Int, c: Int, board: List<MutableList<Boolean>>): Boolean {
-    val topLeft = min(r, c)
-    val topRight = min(r, board.size - 1 - c)
-    for (i in 1..topLeft) {
-        if (board[r - i][c - i]) return false
-    }
-    for (i in 1..topRight) {
-        if (board[r - i][c + i]) return false
-    }
-    for (i in 1..r) {
-        if (board[r - i][c]) return false
+fun isValid(r: Int, c: Int, selected: MutableList<Point>): Boolean {
+    for (idx in 0 until r) {
+        val point = selected[idx]
+        if (c == point.c) return false
+
+        val diffR = abs(r - point.r)
+        val diffC = abs(c - point.c)
+        if (diffR == diffC) return false
     }
 
     return true
