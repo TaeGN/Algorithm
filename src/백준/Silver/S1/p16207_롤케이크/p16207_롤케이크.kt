164803c0669fa9f01319442f8cc5f9fa2c040940
taegn <taegyunn123@gmail.com>
2024-07-03 16:53:05 +0900

    add p16207_롤케이크.kt

---
+++ src/백준/Silver/S1/p16207_롤케이크/p16207_롤케이크.kt
@@ -0,0 +1,40 @@
+package 백준.Silver.S1.p16207_롤케이크
+
+import java.io.StreamTokenizer
+import kotlin.math.min
+
+fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun readInt(): Int {
+        nextToken()
+        return nval.toInt()
+    }
+
+    val N = readInt()
+    val M = readInt()
+    val arr = IntArray(N)
+    repeat(N) { idx ->
+        arr[idx] = readInt()
+    }
+
+    var totalCutCount = 0
+    min(arr.sumOf { it / 10 },
+        M + arr.asSequence()
+            .filter { it % 10 == 0 }
+            .map { it / 10 - 1 }
+            .sorted()
+            .count { curCount -> M >= (totalCutCount + curCount).also { totalCutCount = it } })
+        .let(::println)
+
+//    val sortedArr = arr.asSequence().filter { it % 10 == 0 }.map { it / 10 }.sorted().toList()
+//    var totalCakeCount = 0
+//    for (cakeCount in sortedArr) {
+//        if (totalCutCount + cakeCount - 1 <= M) {
+//            totalCutCount += cakeCount - 1
+//            totalCakeCount += cakeCount
+//        } else {
+//            totalCakeCount += M - totalCutCount
+//            break
+//        }
+//    }
+//    println(totalCakeCount)
+}
