51e0f480d1a8091b287a7a9a4dffc94bb3e76877
taegn <taegyunn123@gmail.com>
2024-07-06 16:55:10 +0900

    add p15317_동방보수.kt

---
+++ src/백준/Gold/G2/p15317_동방보수/p15317_동방보수.kt
@@ -0,0 +1,38 @@
+package 백준.Gold.G2.p15317_동방보수
+
+import java.io.StreamTokenizer
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun readInt(): Int {
+        nextToken()
+        return nval.toInt()
+    }
+
+    val N = readInt()
+    val M = readInt()
+    val X = readInt()
+    val nArr = IntArray(N)
+    repeat(N) { idx ->
+        nArr[idx] = readInt()
+    }
+    nArr.sort()
+
+    val mArr = IntArray(M)
+    repeat(M) { idx ->
+        mArr[idx] = readInt()
+    }
+    mArr.sort()
+
+    fun isPossible(count: Int): Boolean = (0 until count).sumOf { max(0, nArr[it] - mArr[M - count + it]).toLong() } <= X
+    fun maxCount(start: Int = 0, end: Int = min(N, M)): Int {
+        val mid = (start + end) / 2
+        if(start == mid) return if (isPossible(end)) end else start
+        println("$start ~ $end = ${isPossible(mid)}")
+        return if (isPossible(mid)) maxCount(mid, end)
+        else maxCount(start, mid - 1)
+    }
+
+    println(maxCount())
+}
