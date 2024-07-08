0a081fc72aa21f95fbc427ec1b7574c826ecdcae
taegn <taegyunn123@gmail.com>
2024-07-08 13:59:56 +0900

    add 소수출력.kt

---
+++ src/앨리스코드챌린지/떠먹여주는알고리즘/소수출력/소수출력.kt
@@ -0,0 +1,25 @@
+package 앨리스코드챌린지.떠먹여주는알고리즘.소수출력
+
+import kotlin.math.max
+
+fun main() {
+    val N = readln().toInt()
+    fun result(): Int = when (N) {
+        1 -> 2
+        2 -> 3
+        else -> {
+            val isNotPrimeArr = BooleanArray(N)
+            var maxValue = 0
+            for (primeNum in 2 until N) {
+                if (isNotPrimeArr[primeNum]) continue
+                maxValue = max(maxValue, primeNum)
+                for (num in (primeNum * 2) until N step primeNum) {
+                    isNotPrimeArr[num] = true
+                }
+            }
+            maxValue
+        }
+    }
+
+    println(result())
+}
