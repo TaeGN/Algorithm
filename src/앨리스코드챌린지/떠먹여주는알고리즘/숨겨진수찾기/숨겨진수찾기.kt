c0b2a55eb71e38ea873bad25549e3f3043929392
taegn <taegyunn123@gmail.com>
2024-07-10 14:00:15 +0900

    add 숨겨진수찾기.kt

---
+++ src/앨리스코드챌린지/떠먹여주는알고리즘/숨겨진수찾기/숨겨진수찾기.kt
@@ -0,0 +1,19 @@
+package 앨리스코드챌린지.떠먹여주는알고리즘.숨겨진수찾기
+
+fun main() {
+    fun String.sum(): Long {
+        var sum: Long = 0
+        var num: Long = 0
+        for (c in this) {
+            if (c.isDigit()) {
+                num = num * 10 + c.digitToInt()
+            } else {
+                sum += num
+                num = 0
+            }
+        }
+        sum += num
+        return sum
+    }
+    println(readln().sum())
+}
