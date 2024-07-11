1451b74952d69f01759e9485e3d952936ac01d82
taegn <taegyunn123@gmail.com>
2024-07-11 14:34:31 +0900

    add 회문판단

---
+++ src/엘리스코드챌린지/떠먹여주는알고리즘/회문판단/회문판단.java
@@ -0,0 +1,18 @@
+package 엘리스코드챌린지.떠먹여주는알고리즘.회문판단;
+
+import java.io.*;
+
+public class 회문판단 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(isPalindrome(br.readLine()) ? "YES" : "NO");
+    }
+
+    private static boolean isPalindrome(String s) {
+        int length = s.length();
+        for(int i = 0; i < length / 2; i++) {
+            if(s.charAt(i) != s.charAt(length - 1 - i)) return false;
+        }
+        return true;
+    }
+}
