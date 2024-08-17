7d24274a0e6f9f7e7394ce70adba60bfb720127a
taegn <taegyunn123@gmail.com>
2024-08-17 14:02:18 +0900

    update Splitting Items.kt

--- src/Codeforces/Div2/EducationalCodeforces/Round169/C/Splitting Items.kt
+++ src/Codeforces/Div2/EducationalCodeforces/Round169/C/Splitting Items.kt
@@ -1,28 +1,20 @@
 package Codeforces.Div2.EducationalCodeforces.Round169.C
 
-import java.io.StreamTokenizer
 import kotlin.math.min
 
-fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
-    fun nextInt(): Int {
-        nextToken()
-        return nval.toInt()
-    }
-
-    val numArr = IntArray(200000)
+fun main() {
     val sb = StringBuilder()
-    val T = nextInt()
+    val T = readln().toInt()
     repeat(T) {
-        val n = nextInt()
-        var k = nextInt()
-        repeat(n) { idx -> numArr[idx] = nextInt() }
-        numArr.sortDescending(0, n)
+        val (n, k) = readln().split(" ").map(String::toInt)
+        val numArr = readln().split(" ").map(String::toInt).sortedDescending().toIntArray()
         var result = 0L
+        var remainedK = k
         for (i in 0 until n) {
             if (i % 2 == 0) result += numArr[i]
             else {
-                result -= numArr[i] + min(k, numArr[i - 1] - numArr[i])
-                k -= min(k, numArr[i - 1] - numArr[i])
+                result -= numArr[i] + min(remainedK, numArr[i - 1] - numArr[i])
+                remainedK -= min(remainedK, numArr[i - 1] - numArr[i])
             }
         }
         sb.appendLine(result)
