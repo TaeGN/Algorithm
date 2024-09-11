6aa122bc39d53d77d4a723b459822b2d32bdf365
taegn <taegyunn123@gmail.com>
2024-09-12 00:39:00 +0900

    add p11376_열혈강호2.kt

---
+++ src/백준/Platinum/P4/p11376_열혈강호2/p11376_열혈강호2.kt
@@ -0,0 +1,30 @@
+package 백준.Platinum.P4.p11376_열혈강호2
+
+const val EMPTY = -1
+fun main() {
+    val (N, M) = readln().split(" ").map(String::toInt)
+    val matrix = Array(N) { readln().split(" ").map(String::toInt) }
+    val idArr = IntArray(M + 1) { EMPTY }
+    val visited = BooleanArray(M + 1)
+    fun dfs(id: Int): Boolean {
+        for (i in 1 until matrix[id].size) {
+            val job = matrix[id][i]
+            if (visited[job]) continue
+            visited[job] = true
+            if (idArr[job] == EMPTY || dfs(idArr[job])) {
+                idArr[job] = id
+                return true
+            }
+        }
+        return false
+    }
+
+    var count = 0
+    for (i in 0 until N) {
+        repeat(2) {
+            if (dfs(i)) count++
+            visited.fill(false)
+        }
+    }
+    println(count)
+}
