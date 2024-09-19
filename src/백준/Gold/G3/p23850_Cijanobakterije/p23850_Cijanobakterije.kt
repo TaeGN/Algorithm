e3054784dfe9e91ceabb45f8ffca6df3a315ac76
taegn <taegyunn123@gmail.com>
2024-09-19 14:05:37 +0900

    add p23850_Cijanobakterije.kt

--- src/백준/Gold/G3/p23850_Cijanobakterije/p23850_Cijanobakterije.kt
+++ src/백준/Gold/G3/p23850_Cijanobakterije/p23850_Cijanobakterije.kt
@@ -1,61 +1,43 @@
 package 백준.Gold.G3.p23850_Cijanobakterije
 
-import java.io.StreamTokenizer
-import kotlin.math.max
-
-data class Node(val id: Int, val children: MutableSet<Node> = mutableSetOf()) {
-    private var maxDepth = 0
-
-    fun addChild(child: Node) {
-        children.add(child)
+fun main() {
+    val (N, M) = readln().split(" ").map(String::toInt)
+    val parentArr = IntArray(N + 1) { it }
+    val outLists = List(N + 1) { mutableListOf<Int>() }
+    fun find(id: Int): Int = if (parentArr[id] == id) id else find(parentArr[id]).also { parentArr[id] = it }
+    fun union(id1: Int, id2: Int) {
+        parentArr[find(id2)] = find(id1)
     }
-
-    fun maxChainLength(): Int {
-        var first = 0
-        var second = 0
-        for (child in children) {
-            val childMaxDepth = child.maxDepth()
-            if (first < childMaxDepth) {
-                second = first
-                first = childMaxDepth
-            } else if (second < childMaxDepth) {
-                second = childMaxDepth
+    repeat(M) {
+        readln().split(" ").map(String::toInt)
+            .let { union(it[0], it[1]); outLists[it[0]].add(it[1]); outLists[it[1]].add(it[0]) }
+    }
+    var visitedCount = 0
+    val visited = IntArray(N + 1)
+    fun maxLen(id: Int): Pair<Int, Int> {
+        visited[id] = visitedCount
+        var maxLen = 0
+        var maxLenId = id
+        for (nId in outLists[id]) {
+            if (visited[nId] == visitedCount) continue
+            val (nMaxLen, nMaxLenId) = maxLen(nId)
+            if (maxLen < nMaxLen) {
+                maxLen = nMaxLen
+                maxLenId = nMaxLenId
             }
         }
-        return 1 + first + second
+        return (1 + maxLen) to maxLenId
     }
 
-    private fun maxDepth(): Int = if (maxDepth > 0) maxDepth
-    else 1 + (if (children.isNotEmpty()) children.maxOf { it.maxDepth() } else 0).also { maxDepth = it }
-}
-
-fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
-    fun readInt(): Int {
-        nextToken()
-        return nval.toInt()
+    val lenArr = IntArray(N + 1)
+    for (i in 1..N) {
+        val idx = find(i)
+        if (lenArr[idx] != 0) continue
+        visitedCount++
+        val (_, maxLenId) = maxLen(idx)
+        visitedCount++
+        val (maxLen, _) = maxLen(maxLenId)
+        lenArr[idx] = maxLen
     }
-
-    fun IntArray.find(id: Int): Int = if (this[id] == id) id else find(this[id]).also { this[id] = it }
-    fun IntArray.union(id1: Int, id2: Int) {
-        this[find(id1)] = find(id2)
-    }
-
-    val n = readInt()
-    val m = readInt()
-    val nodeList = List(n) { Node(it) }
-    val parentArr = IntArray(n) { it }
-    repeat(m) {
-        val id1 = readInt() - 1
-        val id2 = readInt() - 1
-        nodeList[id1].addChild(nodeList[id2])
-        parentArr.union(id1, id2)
-    }
-
-    val lengthMap = mutableMapOf<Int, Int>()
-    for (id in 0 until n) {
-        val groupId = parentArr.find(id)
-        lengthMap[groupId] = max(lengthMap.getOrDefault(groupId, 0), nodeList[id].maxChainLength())
-    }
-
-    println(lengthMap.values.sum())
+    println(lenArr.sum())
 }
