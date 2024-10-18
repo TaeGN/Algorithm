aa1357e2b797678ffaed71e3e3cb059db9833802
taegn <taegyunn123@gmail.com>
2024-10-19 00:52:57 +0900

    add IdealHolidays.kt

---
+++ src/AtCoder/ProblemList/Difficulty900/IdealHolidays/IdealHolidays.kt
@@ -0,0 +1,21 @@
+package AtCoder.ProblemList.Difficulty900.IdealHolidays
+
+import java.util.TreeSet
+
+fun main() {
+    val (N, A, B) = readln().trim().split(" ").map(String::toInt)
+    val D = readln().trim().split(" ").map(String::toInt)
+    val set = TreeSet<Int>()
+    for (d in D) {
+        set.add(d % (A + B))
+    }
+    fun result(): String {
+        if (set.last() - set.first() < A) return "Yes"
+        for (a in set) {
+            val b = set.higher(a) ?: break
+            if (b - a > B) return "Yes"
+        }
+        return "No"
+    }
+    println(result())
+}
