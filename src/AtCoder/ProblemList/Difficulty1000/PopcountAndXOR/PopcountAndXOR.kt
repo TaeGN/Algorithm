2d6d82877e023dd6ca602fb9d4bd003c4bea2d8a
taegn <taegyunn123@gmail.com>
2024-10-19 15:53:33 +0900

    add PopcountAndXOR.kt

---
+++ src/AtCoder/ProblemList/Difficulty1000/PopcountAndXOR/PopcountAndXOR.kt
@@ -0,0 +1,32 @@
+package AtCoder.ProblemList.Difficulty1000.PopcountAndXOR
+
+fun main() {
+    var (a, b, C) = readln().trim().split(" ").let { Triple(it[0].toInt(), it[1].toInt(), it[2].toLong()) }
+    fun result(): String {
+        var A = 0L
+        var B = 0L
+        for (i in 0 until 60) {
+            if ((C and (1L shl i)) != 0L) {
+                if (a >= b) {
+                    A = A or (1L shl i)
+                    a--
+                } else {
+                    B = B or (1L shl i)
+                    b--
+                }
+            }
+        }
+        if (a != b) return "-1"
+        for (i in 0 until 60) {
+            if ((C and (1L shl i)) == 0L && a > 0 && b > 0) {
+                A = A or (1L shl i)
+                B = B or (1L shl i)
+                a--
+                b--
+            }
+        }
+        if (a != 0 || b != 0) return "-1"
+        return "$A $B"
+    }
+    println(result())
+}
